752d318029b83a68d9f945cf5773e39190be7112
Jordan <jlewis2008@live.com>
2021-05-13 21:36:38 -0400

    late night footer css changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,11 @@
   <div class="footer">
     <a href="index.php"><h5>jordanlewis.dev</h5></a>
-    <a href="blogs.php"><p>Blogs</p></a>
-    <a href="projects.php"><p>My Projects</p></a>
+    <a href="blogs.php" class="footer__link"><p>Blogs</p></a>
+    <a href="projects.php" class="footer__link"><p>My Projects</p></a>
 <?php if($auth->signed_in): ?>
-    <a href="signout.php"><p>Sign Out</p></a>
+    <a href="signout.php" class="footer__link"><p>Sign Out</p></a>
 <?php else: ?>
-    <a href="login.php"><p>Log In</p></a>
+    <a href="login.php" class="footer__link"><p>Log In</p></a>
 <?php endif; ?>
     <p>Copyright by Jordan Lewis &#169; 2021</p>
   </div>
