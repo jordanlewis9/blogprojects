43df2e1474ed91d6ac33247cebae36687dbc2528
Jordan <jlewis2008@live.com>
2021-05-21 10:58:08 -0400

    completed mobile hamburger menu

--- includes/scripts.js
+++ includes/scripts.js
@@ -6,6 +6,40 @@ const navHamburger = document.querySelector(".nav__hamburger");
 const oneLine = document.querySelector(".line-1");
 const twoLine = document.querySelector(".line-2")
 const threeLine = document.querySelector(".line-3");
+let isHamburgerOpen = false;
+
+const throttle = (callback, delay) => {
+  let throttleTimeout = null;
+  let storedEvent = null;
+
+  const throttledEventHandler = event => {
+    storedEvent = event;
+    const shouldHandleEvent = !throttleTimeout;
+
+    if (shouldHandleEvent) {
+      callback(storedEvent);
+      storedEvent = null;
+      throttleTimeout = setTimeout(() => {
+        throttleTimeout = null;
+        if (storedEvent) {
+          throttledEventHandler(storedEvent);
+        }
+      }, delay);
+    }
+  }
+  return throttledEventHandler;
+}
+
+const removeClasses = () => {
+  oneLine.classList.remove("nav__x--top");
+  threeLine.classList.remove("nav__x--bottom");
+  twoLine.classList.remove("nav__hide");
+  navHamburger.classList.remove("nav__hide--border");
+  navbar.classList.remove("nav__expand");
+  navList.classList.remove("nav__list--expand");
+  window.removeEventListener("click", closeMenu);
+  navHamburger.addEventListener("click", openMenu);
+}
 
 const openMenu = (e) => {
     oneLine.classList.add("nav__x--top");
@@ -15,20 +49,27 @@ const openMenu = (e) => {
     navbar.classList.add("nav__expand");
     navList.classList.add("nav__list--expand");
     navHamburger.removeEventListener("click", openMenu);
-    navHamburger.addEventListener("click", closeMenu);
+    setTimeout(() => {
+      window.addEventListener("click", closeMenu);
+      isHamburgerOpen = true;
+    }, 1);
 }
 
 const closeMenu = (e) => {
-  oneLine.classList.remove("nav__x--top");
-  threeLine.classList.remove("nav__x--bottom");
-  twoLine.classList.remove("nav__hide");
-  navHamburger.classList.remove("nav__hide--border");
-  navbar.classList.remove("nav__expand");
-  navList.classList.remove("nav__list--expand");
-  navHamburger.removeEventListener("click", closeMenu);
-  navHamburger.addEventListener("click", openMenu)
+  if (!e.target.closest(".nav") || e.target.closest(".nav__hamburger")) {
+    removeClasses();
+    isHamburgerOpen = false;
+  }
+}
+
+const resizedWindow = e => {
+  if (e.target.innerWidth > 600 && isHamburgerOpen) {
+    removeClasses();
+    isHamburgerOpen = false;
+  }
 }
 
 if (navHamburger) {
-  navHamburger.addEventListener("click", openMenu)
+  navHamburger.addEventListener("click", openMenu);
+  window.addEventListener("resize", throttle(resizedWindow, 100));
 }
